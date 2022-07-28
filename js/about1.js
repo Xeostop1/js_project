e24404baa56ec0ac7e44521b0ce490ff1820e2ab
hanaseo <11reqeuiem27@gmail.com>
2022-07-28 22:05:31 +0900

    js추가 : 전광판기능

--- js/about1.js
+++ js/about1.js
@@ -32,14 +32,8 @@ window.addEventListener("load", function () {
   function ddayView() {
     const gap = today - dday;
     const gapDay = Math.floor(gap / (1000 * 60 * 60 * 24)); //내림 사용. 이건 일만 사용한거네! 1000밀리초=1초 *60 1분 *60 1시간 *24 일
-    const gapHour = String(Math.floor((gap / (1000 * 60 * 60)) % 24)).padStart(
-      2,
-      "0"
-    ); //시
-    const gapMinute = String(Math.floor((gap / (1000 * 60)) % 60)).padStart(
-      2,
-      "0"
-    ); //분
+    const gapHour = String(Math.floor((gap / (1000 * 60 * 60)) % 24)).padStart(2,"0"); //시
+    const gapMinute = String(Math.floor((gap / (1000 * 60)) % 60)).padStart(2,"0"); //분
     const gapSecond = String(Math.floor((gap / 1000) % 60)).padStart(2, "0"); //초 //얘는 나머지로 계산했네.
     dDayDiv.innerHTML = `From 1967-6-18 D+${gapDay} ${gapHour}:${gapMinute}:${gapSecond}`;
     console.log("check");
@@ -47,4 +41,17 @@ window.addEventListener("load", function () {
   ddayView();
   setInterval(ddayView, 1000);
   //setInterval 안된다!!
+//=====================전광판==================
+const marqueeDiv=document.querySelector(".marqueeDiv marquee");
+const singer="LINEUP  David Bowie, James Brown, BB King, Geese Jarrett, Carlos Santana, Etta James, Patty Smith, Massive Attack, Viyok, Radiohead, Leonard Cohen, Deep Purple," 
++"and Prince, Jack White, Questlove, Quincy Jones";
+function marquee(){
+  marqueeDiv.innerText=singer;
+}
+marquee();
+
+
+
+
+  
 });
